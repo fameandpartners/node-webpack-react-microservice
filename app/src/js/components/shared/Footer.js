2a4f97f752980b56882a93be2286b27e743304ec
Adam <elgrecode@gmail.com>
2017-06-20 18:23:23 -0700

    creates footer base content #4

--- app/src/js/components/shared/Footer.js
+++ app/src/js/components/shared/Footer.js
@@ -1,9 +1,39 @@
 import React from 'react';
 
+// CSS
+import '../../../css/components/Footer.scss';
+
+
 const Footer = () => (
-  <footer>
-      Footer stuff goes here
-    </footer>
-  );
+  <footer className="Footer">
+    <div className="layout-container grid-12">
+      <div className="col-12">
+        <h2>Sign up to always enjoy free returns</h2>
+        <form>
+          <input placeholder="Email Address" />
+          <button>Sign up</button>
+        </form>
+      </div>
+      <ul className="col-6">
+        <li className="Footer__category-title">About</li>
+        <li><a href="##">Why shop with us</a></li>
+        <li><a href="##">About us</a></li>
+        <li><a href="##">Fame Society</a></li>
+        <li><a href="##">From our CEO</a></li>
+        <li><a href="##">Privacy Policy</a></li>
+        <li><a href="##">Terms</a></li>
+      </ul>
+      <ul className="col-6">
+        <li className="Footer__category-title">Help</li>
+        <li><a href="##">Shipping Info</a></li>
+        <li><a href="##">Returns Policy</a></li>
+        <li><a href="##">Fame Contact Us</a></li>
+        <li><a href="##">FAQs</a></li>
+        <li><a href="##">Size Guide</a></li>
+        <li><a href="##">Track My Order</a></li>
+      </ul>
+    </div>
+  </footer>
+);
 
 export default Footer;
