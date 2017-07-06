67934a4a4de644df75fc93d05d4b67aa0b57ae49
Adam <elgrecode@gmail.com>
2017-07-05 18:11:15 -0700

    Slider parent component implementing lory.js #20

--- app/src/js/components/pdp/AppMain.js
+++ app/src/js/components/pdp/AppMain.js
@@ -24,19 +24,12 @@ import * as ModalActions from '../../actions/ModalActions';
 import ModalConstants from '../../constants/ModalConstants';
 
 // App Components
+// import ComponentTestPleaseRemove from '../shared/ComponentTestPleaseRemove';
 import HeaderHider from '../shared/HeaderHider';
 import HeaderMobile from '../shared/HeaderMobile';
 import Header from '../shared/Header';
 import Footer from '../shared/Footer';
-
-// UI Global Components
-import Input from '../form/Input';
-import Select from '../form/Select';
-import RadioToggle from '../form/RadioToggle';
-import Button from '../generic/Button';
-
-// OTHER / TODO: REMOVE
-import noop from '../../libs/noop';
+import Slider from '../shared/Slider';
 
 function stateToProps(state) {
   // Which part of the Redux global state does our component want to receive as props?
@@ -114,49 +107,11 @@ class AppMain extends Component {
               :
               <Header />
             }
-            <div className="layout-container typography ui-component-section grid-12">
-              <div className="col-12">
-                <h2>UI Global Components</h2>
-                <button onClick={this.handleActivateModal}>Open Sign Up Modal</button>
-              </div>
-              <div className="col-4">
-                <pre>Input.js</pre>
-                <Input
-                  id="test-input"
-                />
-              </div>
-              <div className="col-4">
-                <pre>Select.js</pre>
-                <Select
-                  id="test-select"
-                  label="Choose Something"
-                  options={[
-                    { id: 0, name: 'Option One', active: false },
-                    { id: 1, name: 'Option Two', active: false },
-                  ]}
-                />
-              </div>
-              <div className="col-4">
-                <pre>RadioToggle.js</pre>
-                <RadioToggle
-                  id="test-select"
-                  label="Choose Something"
-                  value="INCHES"
-                  options={[
-                    { label: 'in', value: 'INCHES' },
-                    { label: 'cm', value: 'CM' },
-                  ]}
-                  onChange={noop}
-                />
-              </div>
-              <hr className="col-12" />
-              <div className="col-4">
-                <pre>Button.js</pre>
-                <Button
-                  text="Fame Button"
-                  handleClick={noop}
-                />
-              </div>
+
+            { /* <ComponentTestPleaseRemove /> */ }
+
+            <div className="layout-container">
+              <Slider />
             </div>
             <div className="App__content layout-container">
               <div className="grid-12">
