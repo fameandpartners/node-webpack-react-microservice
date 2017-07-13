ca0f95359252504d2f77154fae7911b4ce533a20
Adam <elgrecode@gmail.com>
2017-07-13 12:39:13 -0700

    allow slider to have "View Fabric Info" span

--- app/src/js/components/pdp/ProductFabric.js
+++ app/src/js/components/pdp/ProductFabric.js
@@ -1,32 +1,69 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import autoBind from 'react-autobind';
 import fabricImg from '../../../img/test/fabric.png';
+
+// CSS
+import '../../../css/components/ProductFabric.scss';
 
 /* eslint-disable react/prefer-stateless-function */
 class ProductFabric extends PureComponent {
+  constructor(props) {
+    super(props);
+    autoBind(this);
+  }
+
+  handleFabricInfoClick() {
+    const {
+      breakpoint,
+      handleFabricInfoModalClick,
+    } = this.props;
+
+    console.log('this is the current breakpoint in fabric', breakpoint);
+    if (breakpoint === 'mobile') { handleFabricInfoModalClick(); }
+  }
 
   render() {
-    const { fabric, garmentCareInformation } = this.props;
-    console.log('garmentCareInformation', garmentCareInformation);
+    const {
+      fabric,
+      // garmentCareInformation,
+    } = this.props;
     console.log('fabric', fabric);
     return (
       <div
-        className="ProductFabric height--full"
+        className="ProductFabric position--relative height--full"
         style={{ background: `url(${fabricImg})` }}
-      />
+      >
+        <div className="ProductFabric__contents width--full position--absolute">
+          <span
+            className="u-underline"
+            onClick={this.handleFabricInfoClick}
+          >
+              View Fabric Info
+          </span>
+        </div>
+
+      </div>
     );
   }
 }
 
 ProductFabric.propTypes = {
+  breakpoint: PropTypes.string.isRequired,
   fabric: PropTypes.shape({
     id: PropTypes.string,
     smallImg: PropTypes.string,
     name: PropTypes.string,
     description: PropTypes.string,
   }).isRequired,
-  garmentCareInformation: PropTypes.string.isRequired,
+  // garmentCareInformation: PropTypes.string.isRequired,
+  handleFabricInfoModalClick: PropTypes.func.isRequired,
 };
 ProductFabric.defaultProps = {};
 
 export default ProductFabric;
+// <div>
+//   <h4>Garment Care</h4>
+//   <span>{garmentCareInformation}</span>
+// </div>
+// <span>{fabric.description}</span>
