cbc5b8d283272a608322aae07374b2626f0ecfc5
Adam <elgrecode@gmail.com>
2017-08-22 14:55:19 -0700

    finishes query string params generation on customization selection

--- app/src/js/reducers/AppReducer.js
+++ app/src/js/reducers/AppReducer.js
@@ -1,6 +1,5 @@
 import Immutable from 'immutable';
 import queryString from 'query-string';
-import { assign } from 'lodash';
 import AppConstants, { QUERY_PARAMS } from '../constants/AppConstants';
 import win from '../polyfills/windowPolyfill';
 
@@ -9,7 +8,10 @@ export const $$initialState = Immutable.fromJS({
   siteVersion: 'us',
   sideMenuOpen: false,
   queryStr: null,
-  queryParams: {},
+  queryParams: {
+    [QUERY_PARAMS.color]: undefined,
+    [QUERY_PARAMS.customizations]: undefined,
+  },
 });
 
 function generateQueryParms(queryUpdateObj) {
@@ -18,22 +20,28 @@ function generateQueryParms(queryUpdateObj) {
 
 function setURLQueryParams(queryStr) {
   if (win.history.pushState) {
-    const newurl = `${win.location.protocol}//${win.location.host}${win.location.pathname}?${queryStr}`;
+    const baseUrl = `${win.location.protocol}//${win.location.host}${win.location.pathname}`;
+    const baseQuery = queryStr ? `?${queryStr}` : '';
+    const newUrl = baseUrl + baseQuery;
     // TODO: @elgrecode IF we don't want to add a history state we can use replaceState
-    win.history.pushState({ path: newurl }, '', newurl);
+    win.history.pushState({ path: newUrl }, '', newUrl);
   }
 }
 
 export default function AppReducer($$state = $$initialState, action = null) {
   switch (action.type) {
     case AppConstants.SET_SHAREABLE_QUERY_PARAMS: {
-      const queryParams = assign({}, {
-        [QUERY_PARAMS.color]: action.color,
-        [QUERY_PARAMS.customizations]: action.customizations,
-      });
-      const queryStr = generateQueryParms(queryParams);
+      const oldQueryParams = $$state.get('queryParams').toJS();
+      const newQueryParams = {
+        [QUERY_PARAMS.color]: action.color
+          || oldQueryParams[QUERY_PARAMS.color],
+        [QUERY_PARAMS.customizations]: action.customizations
+          || oldQueryParams[QUERY_PARAMS.customizations],
+      };
+      const queryStr = generateQueryParms(newQueryParams);
       setURLQueryParams(queryStr);
-      return $$state.merge({ queryParams, queryStr });
+
+      return $$state.merge({ queryParams: newQueryParams, queryStr });
     }
     case AppConstants.ACTIVATE_SIDE_MENU: {
       return $$state.merge({
