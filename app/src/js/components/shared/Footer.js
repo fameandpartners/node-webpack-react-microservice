54dd167183d37c26a2f023b34b230fca49588c57
Michael  Gnanakone <michaelg@Michaels-MacBook-Pro.local>
2017-08-24 15:07:02 -0700

    Almost done with the footer, need some icons though.

--- app/src/js/components/shared/Footer.js
+++ app/src/js/components/shared/Footer.js
@@ -1,89 +1,202 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import autoBind from 'react-autobind';
-
-// OTHER / TODO: REMOVE
-import noop from '../../libs/noop';
-import objnoop from '../../libs/objnoop';
-
-// Constants
-import ModalConstants from '../../constants/ModalConstants';
-
-// Constants
-import ModalActions from '../../actions/ModalActions';
-
+import classNames from 'classnames';
 // Components
 import Button from '../generic/Button';
 import Input from '../form/Input';
+import Icon from './Icon';
 
 // CSS
 import '../../../css/components/Footer.scss';
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(ModalActions, dispatch);
-}
 
 /* eslint-disable react/prefer-stateless-function */
 class Footer extends PureComponent {
   constructor(props) {
     super(props);
     autoBind(this);
+    this.state = {
+      siteVersion: 'US',
+      signupError: false,
+    };
   }
-
   handleSignupClick(e) {
     e.preventDefault();
-    this.props.activateModal({ modalId: ModalConstants.SIGN_UP_MODAL });
+    // Simulate signup error
+    this.setState({
+      signupError: true,
+    });
+  }
+  changeSiteVersion() {
+    // Presentaional only, will actually change stuff in the future
+    const { siteVersion } = this.state;
+    if (siteVersion === 'AU') {
+      this.setState({
+        siteVersion: 'US',
+      });
+    } else {
+      this.setState({
+        siteVersion: 'AU',
+      });
+    }
   }
 
   render() {
+    const { siteVersion, signupError } = this.state;
     return (
       <footer className="Footer">
-        <div className="layout-container grid-noGutter-12">
-          <div className="col-12">
-            <h2>Sign up to always enjoy free returns</h2>
-            <form>
-              <div className="display--inline-block">
-                <Input id="footer-email" placeholder="Email Address" />
+        <div className="layout-container grid-noGutter-reverse-spaceAround">
+          <ul className="col-2_sm-4 Footer__category-list">
+            <li>
+              <p className="Footer__category-title">Help</p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Shipping Info</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Returns Policy</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Fame Contact Us</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">FAQs</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Size Guide</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Track My Order</a>
+              </p>
+            </li>
+          </ul>
+          <ul className="col-2_sm-4 Footer__category-list">
+            <li><p className="Footer__category-title">About</p></li>
+            <li>
+              <p>
+                <a href="##">Why shop with us</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">About us</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Fame Society</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">From our CEO</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Privacy Policy</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Terms</a>
+              </p>
+            </li>
+          </ul>
+          <ul className="col-2_sm-4_xs-0 Footer__category-list">
+            <li><p className="Footer__category-title">Shop By</p></li>
+            <li>
+              <p>
+                <a href="##">Best Sellers</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">What's new</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Made in 48 Hours</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Formal</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">Prom</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="##">View All Dresses</a>
+              </p>
+            </li>
+          </ul>
+          <div className="col-6_md-12_sm-12_sm-first">
+            <p className="Footer__copy">
+              Sign up to always enjoy free returns
+            </p>
+            <form
+              className={
+                classNames({
+                  'Footer__form--error': signupError,
+                  'grid-center': true,
+                  Footer__form: true,
+
+                })
+              }
+            >
+              <div className="col-7_sm-9 padding--none">
+                <div>
+                  <Input
+                    id="footer-email"
+                    placeholder="Email your email address"
+                    type="email"
+                    error={signupError}
+                    inlineMeta={signupError ? 'Error! Something is wrong...' : null}
+                  />
+                </div>
               </div>
-              <div className="display--inline-block">
-                <Button handleClick={noop} text="Sign up" />
+              <div className="col-3 padding--none">
+                <Button
+                  className="padding--none"
+                  handleClick={this.handleSignupClick}
+                  text="Sign up"
+                />
               </div>
-              <Button handleClick={this.handleSignupClick} text="Test Sign up" />
             </form>
           </div>
-          <ul className="col-6">
-            <li className="Footer__category-title">About</li>
-            <li><a href="##">Why shop with us</a></li>
-            <li><a href="##">About us</a></li>
-            <li><a href="##">Fame Society</a></li>
-            <li><a href="##">From our CEO</a></li>
-            <li><a href="##">Privacy Policy</a></li>
-            <li><a href="##">Terms</a></li>
-          </ul>
-          <ul className="col-6">
-            <li
-              className="Footer__category-title"
-            >
-      Help
-      </li>
-            <li><a href="##">Shipping Info</a></li>
-            <li><a href="##">Returns Policy</a></li>
-            <li><a href="##">Fame Contact Us</a></li>
-            <li><a href="##">FAQs</a></li>
-            <li><a href="##">Size Guide</a></li>
-            <li><a href="##">Track My Order</a></li>
-          </ul>
         </div>
+        <div className="layout-container grid-noGutter">
+          <div className="col-12 Footer__site-version-container">
+            <p className="text--noHighlight">Country: &nbsp;
+              <span
+                className="text--underline cursor--pointer"
+                onClick={this.changeSiteVersion}
+              >
+                {siteVersion}
+              </span>
+            </p>
+          </div>
+        </div>
+        <Icon />
       </footer>
     );
   }
 }
 
-Footer.propTypes = {
-  // Redux Actions
-  activateModal: PropTypes.func.isRequired,
-};
-
-export default connect(objnoop, mapDispatchToProps)(Footer);
+export default Footer;
