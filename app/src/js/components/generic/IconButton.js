a924d259bc3c6aee26f6793cd176dfd3192ff130
Nav Samra <navsamra@me.com>
2017-08-25 10:01:14 -0700

    make IconButton.js a PureComponent

--- app/src/js/components/generic/IconButton.js
+++ app/src/js/components/generic/IconButton.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import autobind from 'react-autobind';
-import classNames from 'classnames';
 
 // Components
 import IconSVG from '../generic/IconSVG';
@@ -9,16 +7,10 @@ import IconSVG from '../generic/IconSVG';
 // CSS
 import '../../../css/components/IconButton.scss';
 
-class IconButton extends Component {
-  constructor(props) {
-    super(props);
-
-    autobind(this);
-  }
-
+/* eslint-disable react/prefer-stateless-function */
+class IconButton extends PureComponent {
   render() {
     const {
-      className,
       handleClick,
       svgPath,
       width,
@@ -28,12 +20,7 @@ class IconButton extends Component {
     return (
       <button
         onClick={handleClick}
-        className={
-          classNames(
-            'IconButton',
-            className,
-          )
-        }
+        className="IconButton"
       >
         <IconSVG
           svgPath={svgPath}
@@ -48,13 +35,11 @@ class IconButton extends Component {
 IconButton.propTypes = {
   handleClick: PropTypes.func.isRequired,
   svgPath: PropTypes.string.isRequired,
-  className: PropTypes.string,
   width: PropTypes.string,
   height: PropTypes.string,
 };
 
 IconButton.defaultProps = {
-  className: '',
   width: '40px',
   height: '40px',
 };
