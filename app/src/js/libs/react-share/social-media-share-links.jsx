370dea2e9fbbe53e257f3c8ec96fd6afdc7f9b80
Nav Samra <navsamra@me.com>
2017-08-25 10:31:16 -0700

    remove assert

--- app/src/js/libs/react-share/social-media-share-links.jsx
+++ app/src/js/libs/react-share/social-media-share-links.jsx
@@ -1,13 +1,8 @@
 /* eslint-disable prefer-template */
-import assert from 'assert';
-
 import { objectToGetParams } from './utils';
 
 
 export function twitter(url, { title, via, hashtags = [] }) {
-  assert(url, 'twitter.url');
-  assert(Array.isArray(hashtags), 'twitter.hashtags is not an array');
-
   return 'https://twitter.com/share' + objectToGetParams({
     url,
     text: title,
@@ -17,8 +12,6 @@ export function twitter(url, { title, via, hashtags = [] }) {
 }
 
 export function facebook(url, { quote, hashtag }) {
-  assert(url, 'facebook.url');
-
   return 'https://www.facebook.com/sharer/sharer.php' + objectToGetParams({
     u: url,
     quote,
@@ -27,9 +20,6 @@ export function facebook(url, { quote, hashtag }) {
 }
 
 export function pinterest(url, { media, description }) {
-  assert(url, 'pinterest.url');
-  assert(media, 'pinterest.media');
-
   return 'https://pinterest.com/pin/create/button/' + objectToGetParams({
     url,
     media,
