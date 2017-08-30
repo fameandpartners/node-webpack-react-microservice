375dab321b218cb32f998171041fa473b323758d
Nav Samra <navsamra@me.com>
2017-08-30 00:13:22 -0700

    rename classes (about to rename components)

--- app/src/js/components/pdp/ViewSizeGuideInfo.js
+++ app/src/js/components/pdp/ViewSizeGuideInfo.js
@@ -13,47 +13,47 @@ class ViewSizeGuideInfo extends PureComponent {
     } = this.props;
 
     return (
-      <div className="ViewSizeGuideInfo__table">
-        <div className="ViewSizeGuideInfo__table-column">
-          <div className="ViewSizeGuideInfo__table-cell">
+      <div className="SizeGuideTable">
+        <div className="SizeGuideTable__column">
+          <div className="SizeGuideTable__cell">
             US
           </div>
-          <div className="ViewSizeGuideInfo__table-cell">
+          <div className="SizeGuideTable__cell">
             AU
           </div>
-          <div className="ViewSizeGuideInfo__table-cell">
+          <div className="SizeGuideTable__cell">
             Bust
           </div>
-          <div className="ViewSizeGuideInfo__table-cell">
+          <div className="SizeGuideTable__cell">
             Underbust
           </div>
-          <div className="ViewSizeGuideInfo__table-cell">
+          <div className="SizeGuideTable__cell">
             Waist
           </div>
-          <div className="ViewSizeGuideInfo__table-cell">
+          <div className="SizeGuideTable__cell">
             Hip
           </div>
         </div>
         {sizeChart.map(
           (item, key) =>
             <div key={`replace-with-actual-key-${key}`}>
-              <div className="ViewSizeGuideInfo__table-column">
-                <div className="ViewSizeGuideInfo__table-cell">
+              <div className="SizeGuideTable__column">
+                <div className="SizeGuideTable__cell">
                   {item['Size US']}
                 </div>
-                <div className="ViewSizeGuideInfo__table-cell">
+                <div className="SizeGuideTable__cell">
                   {item['Size Aus/UK']}
                 </div>
-                <div className="ViewSizeGuideInfo__table-cell">
+                <div className="SizeGuideTable__cell">
                   {centimeters ? item['Bust cm'] : item['Bust Inches']}
                 </div>
-                <div className="ViewSizeGuideInfo__table-cell">
+                <div className="SizeGuideTable__cell">
                   {centimeters ? item['Underbust cm'] : item['Underbust Inches']}
                 </div>
-                <div className="ViewSizeGuideInfo__table-cell">
+                <div className="SizeGuideTable__cell">
                   {centimeters ? item['Waist cm'] : item['Waist Inches']}
                 </div>
-                <div className="ViewSizeGuideInfo__table-cell">
+                <div className="SizeGuideTable__cell">
                   {centimeters ? item['Hip cm'] : item['Hip Inches']}
                 </div>
               </div>
