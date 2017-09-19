7ac1b5437bc78c88fbf0a5f60c8bffa6637e3662
Thanh Truong <thanht@fameandpartners.com>
2017-09-19 16:59:22 -0700

    fixy fixy

--- build-dev.sh
+++ build-dev.sh
@@ -3,7 +3,7 @@ set -e
 npm install -g npm
 cd app
 npm install
-set PUBLIC_URL=https://content-dev.fameandgroups.com/
+export PUBLIC_URL=https://content-dev.fameandgroups.com/
 echo $PUBLIC_URL
 npm run build
 echo 'build it...and they will come'
