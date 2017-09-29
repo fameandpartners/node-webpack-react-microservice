631ce560731396cb03b3df6a0a549aea07b4210d
Adam <elgrecode@gmail.com>
2017-09-28 18:03:32 -0700

    clean up before PR #149

--- app/config/webpack.config.prod.js
+++ app/config/webpack.config.prod.js
@@ -7,7 +7,6 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
-// const SpriteLoaderPlugin = require('svg-sprite-loader/plugin');
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -175,15 +174,6 @@ module.exports = {
           {
             loader: 'babel-loader',
           },
-          // {
-          //   loader: 'svg-sprite-loader',
-          //   options: {
-          //     extract: true,
-          //     runtimeOptions: {
-          //       spriteFilename: 'sprite-[hash:6].svg',
-          //     },
-          //   },
-          // },
         ],
       },
       // ** STOP ** Are you adding a new loader?
@@ -239,8 +229,6 @@ module.exports = {
     extractSass,
     // Uglifies/Minifies CSS
     new OptimizeCssAssetsPlugin(),
-    // Creates Sprite SVG
-    // new SpriteLoaderPlugin(),
     // Generate a manifest file which contains a mapping of all asset filenames
     // to their corresponding output file so that tools can pick it up without
     // having to parse `index.html`.
--- app/src/js/components/generic/LoadingSpinner.js
+++ app/src/js/components/generic/LoadingSpinner.js
@@ -1,6 +1,4 @@
 import React, { PureComponent } from 'react';
-// import PropTypes from 'prop-types';
-// import classnames from 'classnames';
 
 // CSS
 import '../../../css/components/LoadingSpinner.scss';
@@ -13,9 +11,5 @@ class LoadingSpinner extends PureComponent {
     );
   }
 }
-
-LoadingSpinner.propTypes = {};
-
-LoadingSpinner.defaultProps = {};
 
 export default LoadingSpinner;
