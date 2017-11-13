bad8bb9d73edd46405836a256b8b711158aca910
Nav Samra <navsamra@me.com>
2017-11-13 15:12:40 -0800

    update CartEmpty state styling

--- app/src/js/components/shared/cart/CartEmpty.js
+++ app/src/js/components/shared/cart/CartEmpty.js
@@ -1,11 +1,14 @@
 import React, { PureComponent } from 'react';
 
+// CSS
+import '../../../../css/components/CartEmpty.scss';
+
 /* eslint-disable react/prefer-stateless-function */
 class CartEmpty extends PureComponent {
   render() {
     return (
       <div className="CartEmpty">
-        Nothing to see here. Your cart is empty!
+        Your bag is empty
       </div>
     );
   }
