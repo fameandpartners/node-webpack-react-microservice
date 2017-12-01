aa073280fe68394bda31ce21b594926bc0b73227
Nav Samra <navsamra@me.com>
2017-12-01 13:28:47 -0800

    fix customizations display

--- app/src/js/components/flash_sale/FlashProductDescription.js
+++ app/src/js/components/flash_sale/FlashProductDescription.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 
 // Utilities
 import { formatCents } from '../../utilities/accounting';
-// import { addonSelectionDisplayText } from '../../utilities/pdp';
 
 // CSS
 import '../../../css/components/FlashProductDescription.scss';
@@ -14,17 +13,14 @@ class FlashProductDescription extends Component {
   constructor(props) {
     super(props);
     autoBind(this);
+
+    this.state = {
+      customizations: [],
+    };
   }
 
   calculateInstallment(divisor, currencySymbol) {
     return currencySymbol + (Number(this.calculateSubTotal('')) / divisor).toFixed(2);
-  }
-
-  generateCustomizationText() {
-    return '[Customization info goes here Nav, I think you can just throw in the customizations into the function below]';
-    // const { lineItem } = this.props;
-    // console.log('lineItem', lineItem);
-    // return addonSelectionDisplayText({ selectedAddonOptions: lineItem.customizations });
   }
 
   generateFlashColorSwatch() {
@@ -43,9 +39,24 @@ class FlashProductDescription extends Component {
     };
   }
 
+  setCustomizations() {
+    const { lineItem } = this.props;
+
+    if (lineItem.customisations.length) {
+      this.setState({
+        customizations: lineItem.customisations,
+      });
+    }
+  }
+
+  componentWillMount() {
+    this.setCustomizations();
+  }
+
   render() {
     const { lineItem } = this.props;
     const presentation = lineItem.color_presentation;
+    const { customizations } = this.state;
 
     return (
       <div className="FlashProductDescription typography u-center">
@@ -69,7 +80,19 @@ class FlashProductDescription extends Component {
         </p>
 
         <div className="FlashProductDescription__customization-details u-mt-normal">
-          <p>{this.generateCustomizationText()}</p>
+          { customizations.length ?
+            (
+              <div className="FlashProductDescription__addons-details">
+                <h5 className="FlashProductDescription__addons-header">
+                  Customizations
+                </h5>
+                <ul className="FlashProductDescription__addons-list">
+                  {/* eslint-disable react/no-array-index-key */}
+                  {customizations.map((c, i) => <li key={i}>{c}</li>)}
+                </ul>
+              </div>
+            ) : null
+          }
           <p>Color: {presentation}</p>
           <div
             style={this.generateFlashColorSwatch()}
