98a3a591075d04c1c82105e246b5f644db3638f8
Adam <elgrecode@gmail.com>
2017-12-05 11:46:05 -0800

    hover images

--- app/src/js/components/flash_sale/FlashSaleProductGrid.js
+++ app/src/js/components/flash_sale/FlashSaleProductGrid.js
@@ -33,18 +33,20 @@ class FlashSaleProductGrid extends Component {
             key={dress.id}
             className="FlashSaleProduct__container col-4_sm-6"
           >
-            <a href={`/sample-sale/${dress.id}`} className="FlashSaleProduct__image-wrapper">
-              <img
-                className="FlashSaleProduct__image--original"
-                alt={dress.name}
-                src={dress.images[dress.images.length - 1]}
-              />
-              <img
-                className="FlashSaleProduct__image--hover"
-                alt={dress.name}
-                src={dress.images[dress.images.length - 2]}
-              />
-            </a>
+            <div className="FlashSaleProduct__image-wrapper">
+              <a className="u-display--inline-block" href={`/sample-sale/${dress.id}`}>
+                <img
+                  className="FlashSaleProduct__image FlashSaleProduct__image--original"
+                  alt={dress.name}
+                  src={dress.images[dress.images.length - 1]}
+                />
+                <img
+                  className="FlashSaleProduct__image FlashSaleProduct__image--hover"
+                  alt={dress.name}
+                  src={dress.images[dress.images.length - 2]}
+                />
+              </a>
+            </div>
             <div className="FlashSaleProduct__info grid-12">
               <div className="col-8">
                 <a href={`/sample-sale/${dress.id}`}>
