b4f0e6bf1f9d6609d162336908ba89c8ceddce32
Matthew Whittaker <mwhitt89@gmail.com>
2017-12-10 16:50:24 -0500

    wip: display body measurements on overview

--- app/src/js/components/size-profile/FitIDOverview.js
+++ app/src/js/components/size-profile/FitIDOverview.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 // Actions
+// import SizeProfileActions from '../../actions/SizeProfileActions';
 import WizardActions from '../../actions/WizardActions';
 
 // Constants
@@ -14,8 +15,15 @@ import WizardConstants from '../../constants/WizardConstants';
 import Button from '../generic/Button';
 import WizardStep from '../wizard/WizardStep';
 
-function stateToProps() {
-  return {};
+function stateToProps(state) {
+  return {
+    temporaryHeightValue: state.$$sizeProfileState.get('temporaryHeightValue'),
+    temporaryWeightValue: state.$$sizeProfileState.get('temporaryWeightValue'),
+    temporaryAgeValue: state.$$sizeProfileState.get('temporaryAgeValue'),
+    // temporaryBustValue: state.$$sizeProfileState.get('temporaryBustValue'),
+    // temporaryWaistValue: state.$$sizeProfileState.get('temporaryWaistValue'),
+    // temporaryHipValue: state.$$sizeProfileState.get('temporaryHipValue'),
+  };
 }
 
 function dispatchToProps(dispatch) {
@@ -42,19 +50,51 @@ class FitIDOverview extends Component {
   }
 
   render() {
+    const {
+      temporaryHeightValue,
+      temporaryWeightValue,
+      temporaryAgeValue,
+    } = this.props;
+
     return (
       <WizardStep
         handlePreviousStep={this.handlePreviousStep}
         handleCloseWizard={this.handleCloseWizard}
-        modalClassName="full-padding-big u-flex u-flex--1"
+        modalClassName="u-flex u-flex--1"
         modalContentClassName="u-width--full u-overflow-y--scroll"
         modalWrapperClassName="u-flex--col"
       >
         <div className="FitIDOverview">
           <h3 className="WizardStep__title u-mb-normal u-mt-big">
-            Let&rsquo;s make sure we got this correct.
+            Let&rsquo;s make sure we <br /> got this correct.
           </h3>
-          <div className="ButtonBox--medium-width ButtonBox--center">
+          <div className="FitIDOverview__body-fit-wrapper">
+            <div className="grid-center-noGutter">
+              <ul className="FitIDOverview__body-fit-list">
+                <li>
+                  <span className="body-fit-title">Height</span>
+                  <span className="body-fit-value">{temporaryHeightValue}</span>
+                </li>
+                <li>
+                  <span className="body-fit-title">Weight</span>
+                  <span className="body-fit-value">{temporaryWeightValue}</span>
+                </li>
+                <li>
+                  <span className="body-fit-title">Age</span>
+                  <span className="body-fit-value">{temporaryAgeValue}</span>
+                </li>
+                <li>
+                  <span className="body-fit-title">Bra</span>
+                  <span className="body-fit-value">34B</span>
+                </li>
+                <li>
+                  <span className="body-fit-title">Jeans</span>
+                  <span className="body-fit-value">26</span>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="ButtonBox ButtonBox--center">
             <Button
               className="FitIDOverview__button"
               text="Save your Fit I.D. to your profile"
@@ -68,10 +108,18 @@ class FitIDOverview extends Component {
 }
 
 FitIDOverview.propTypes = {
+  // Redux Actions
   jumpToStep: PropTypes.func.isRequired,
+  // Redux Props
+  temporaryHeightValue: PropTypes.number,
+  temporaryWeightValue: PropTypes.string,
+  temporaryAgeValue: PropTypes.string,
 };
 
 FitIDOverview.defaultProps = {
+  temporaryHeightValue: null,
+  temporaryWeightValue: null,
+  temporaryAgeValue: null,
 };
 
 export default connect(stateToProps, dispatchToProps)(FitIDOverview);
