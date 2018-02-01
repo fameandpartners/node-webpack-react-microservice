9ae0993ccaae218804ca18616bbd0a24586ebd92
Mateus Moraes <mateus@mmoraes.net>
2018-02-01 00:43:19 -0200

    update header section markup to accommodate live text

--- app/src/js/components/super_collection/Header.js
+++ app/src/js/components/super_collection/Header.js
@@ -23,20 +23,34 @@ class Header extends Component {
     const { header } = this.props;
 
     return (
-      <div className="grid-noGutter">
-        <div className="col-12">
+      <div className="SuperCollection-Header u-overlay-area">
+        <a href={header.url}>
           <picture>
-            <source srcSet={header.img} media="(min-width: 1200px)" />
-            <source srcSet={header.img_tablet} media="(min-width: 768px) and (max-width: 1199px)" />
-            <source srcSet={header.img_mobile} media="(max-width: 767px)" />
+            <source srcSet={header.img} media="(min-width: 1025px)" />
+            <source srcSet={header.img_tablet} media="(min-width: 577px) and (max-width: 1024px)" />
+            <source srcSet={header.img_mobile} media="(max-width: 576px)" />
             <img
               src={header.img}
               srcSet={header.img}
               alt="Buy custom bridesmaid dresses"
-              className="SuperCollection-Header--img"
+              className="SuperCollection-Header--img u-overlay-area__media"
             />
           </picture>
-        </div>
+          <div className="SuperCollection-Header--ctaWrapper u-overlay-area__overlay">
+            <div className="grid grid-noGutter SuperCollection-Header--grid">
+              <div className="col-4_md-8_sm-10_xs-10" data-push-left="off-1_md-2_sm-1_xs-1">
+                <span className="SuperCollection-Header--cta">
+                  <h1 className="SuperCollection-Header--ctaHeading">
+                    Millions of options.
+                  </h1>
+                  <h1 className="SuperCollection-Header--ctaHeading">
+                    Only one you.
+                  </h1>
+                </span>
+              </div>
+            </div>
+          </div>
+        </a>
       </div>
     );
   }
