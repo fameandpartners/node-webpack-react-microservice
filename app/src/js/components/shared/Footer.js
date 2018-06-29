46ba53a106b28323076691beef06161210f02471
Mateus Moraes <mateus@mmoraes.net>
2018-06-29 15:09:17 -0300

    update faqs links (footer)

--- app/src/js/components/shared/Footer.js
+++ app/src/js/components/shared/Footer.js
@@ -33,14 +33,14 @@ class Footer extends PureComponent {
             </li>
             <li>
               <p>
-                <a className="link--static" href="/faqs#collapse-delivery-how-long">
+                <a className="link--static" href="/faqs#collapse-how-long-does-delivery-take">
                   Shipping Info
                 </a>
               </p>
             </li>
             <li>
               <p>
-                <a className="link--static" href="/faqs#collapse-returns-policy">Returns Policy</a>
+                <a className="link--static" href="/faqs#collapse-what-is-our-returns-policy">Returns Policy</a>
               </p>
             </li>
             <li>
@@ -60,7 +60,7 @@ class Footer extends PureComponent {
             </li>
             <li>
               <p>
-                <a className="link--static" href="/faqs#collapse-orders-track">Track My Order</a>
+                <a className="link--static" href="/faqs#collapse-how-can-i-track-my-order">Track My Order</a>
               </p>
             </li>
             <li>
